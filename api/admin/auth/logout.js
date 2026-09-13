3a68348170a8754566c40e396da5b53b948930de
MidedBrown <mathewmidedzi@gmail.com>
2026-09-13 06:09:42 +0000

    Create logout.js

---
+++ api/admin/auth/logout.js
@@ -0,0 +1,38 @@
+const {
+  getAdminSessionToken,
+  destroyAdminSession,
+  clearAdminSessionCookie
+} = require("../../../lib/admin-auth");
+
+const {
+  sendError,
+  requireMethod,
+  applySecurityHeaders,
+  logServerError
+} = require("../../../lib/security");
+
+module.exports = async (req, res) => {
+  applySecurityHeaders(res);
+
+  if (!requireMethod(req, res, "POST")) {
+    return;
+  }
+
+  try {
+    const token = getAdminSessionToken(req);
+
+    if (token) {
+      destroyAdminSession(token);
+    }
+
+    clearAdminSessionCookie(res);
+
+    return res.status(200).json({
+      success: true,
+      message: "Admin logout successful."
+    });
+  } catch (error) {
+    logServerError("POST /api/admin/auth/logout failed", error);
+    return sendError(res, 500, "Unable to complete admin logout.");
+  }
+};
